b6d555dbbad04e769b5f627ee230b37a89dec62c
rahulkhott <111186413+rahulkhott@users.noreply.github.com>
2023-12-14 23:20:57 +0530

    Update and rename AmazonTest.java to BasePage.java

---
+++ BasePage.java
@@ -0,0 +1,19 @@
+
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    protected static WebDriver driver;
+
+    public static void setDriver(WebDriver webDriver) {
+        driver = webDriver;
+    }
+
+    public static void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
